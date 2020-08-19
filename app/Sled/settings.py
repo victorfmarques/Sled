fecba03c61019e27c3ded018020fb33dab680c82
Victor Felisbino Marques <victorfmarques@outlook.com>
2020-08-19 18:35:10 -0300

    Custom User model created

--- app/Sled/settings.py
+++ app/Sled/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'f6!eh@$w2vro0#a0fyy91vgnzb0)bo02l^n&k)b2tw0l1!9s91'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core'
 ]
 
 MIDDLEWARE = [
@@ -118,3 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'core.User'
---
+++ app/core/test_models.py
@@ -0,0 +1,60 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+    ''' Class designed to test models from core app '''
+
+    def test_create_user_with_email_successful(self):
+        ''' Test creating a new user with a email '''
+
+        email = 'admin@admin.com'
+        password = 'password123'
+        name = 'Admin'
+        last_name = 'admin'
+
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password,
+            name=name,
+            last_name=last_name
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+        self.assertEqual(user.name, name)
+        self.assertEqual(user.last_name, last_name)
+
+    def test_create_user_with_normalized_email(self):
+        ''' Test creating a user with its email normalized '''
+
+        email = 'admin@ADMIN.com'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password='password123',
+            name='name',
+            last_name='last_name'
+        )
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        ''' Test creating a user with a invalid email '''
+
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(
+                email=None,
+                password='password123',
+                name='name',
+                last_name='last_name'
+            )
+
+    def test_create_super_user(self):
+        ''' Test creating a superuser '''
+
+        user = get_user_model().objects.create_superuser(
+            email='super@user.com',
+            password='superpassword123'
+        )
+
+        self.assertTrue(user.is_staff)
+        self.assertTrue(user.is_superuser)
